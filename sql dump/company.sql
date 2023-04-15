f3f5d0c8decafc0be356a2c1ea7f86340d2a81bb
MartinkPatel <94983670+MartinkPatel@users.noreply.github.com>
2023-04-15 14:10:37 +0530

    added job post

--- sql dump/company.sql
+++ sql dump/company.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2023 at 09:03 PM
+-- Generation Time: Apr 15, 2023 at 10:39 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -47,6 +47,28 @@ CREATE TABLE `company` (
 INSERT INTO `company` (`cid`, `name`, `email`, `phone`, `location`, `password`, `cbio`, `size`, `ctc`, `verify`) VALUES
 (4, 'abcd', 'abcd@gmail.com', 2147483647, 'surat', 'Abcd@1234', 'sdsd', 54, 55, 0);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobpost`
+--
+
+CREATE TABLE `jobpost` (
+  `jid` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `phone` int(11) NOT NULL,
+  `email` text NOT NULL,
+  `position` text NOT NULL,
+  `branch` text NOT NULL,
+  `minq` int(11) NOT NULL,
+  `minm` float NOT NULL,
+  `moi` varchar(10) NOT NULL,
+  `toi` varchar(10) NOT NULL,
+  `ctc` float NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -58,6 +80,13 @@ ALTER TABLE `company`
   ADD PRIMARY KEY (`cid`);
 
 --
+-- Indexes for table `jobpost`
+--
+ALTER TABLE `jobpost`
+  ADD PRIMARY KEY (`jid`),
+  ADD KEY `company_fk` (`cid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -66,6 +95,22 @@ ALTER TABLE `company`
 --
 ALTER TABLE `company`
   MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `jobpost`
+--
+ALTER TABLE `jobpost`
+  MODIFY `jid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `jobpost`
+--
+ALTER TABLE `jobpost`
+  ADD CONSTRAINT `company_fk` FOREIGN KEY (`cid`) REFERENCES `company` (`cid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
